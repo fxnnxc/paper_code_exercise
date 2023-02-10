4ff32953be1138142a4ded4648badc5d78f7166f
fxnnxc <bumjin@kaist.ac.kr>
2023-02-10 10:39:19 +0900

    remove irrelevant lines#

--- cnn_classifier/tensorboard_extract.py
+++ cnn_classifier/tensorboard_extract.py
@@ -17,15 +17,3 @@ def parse_tfevent(tfevent):
         step=tfevent.step,
         value=float(tfevent.summary.value[0].simple_value),
     )
-
-if __name__ == "__main__":    
-    runs=None 
-    alls=[] 
-    for i, run in enumerate(runs):
-            a = convert_tfevent(run)
-            a['seed'] = i
-            a['model'] = {"anneal_dist":"dist", "anneal_pred":"pred", "baseline": "baseline"}[model]
-            alls.append(a[a['name']=='CReQuaL/entropy_1'])
-            alls.append(a[a['name']=='CReQuaL/entropy_0'])
-    df = pd.concat(alls)
-    df['code'] = df['name'].apply(lambda x : x[-1])
